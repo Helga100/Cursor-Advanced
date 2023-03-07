53dbba9f9e205c87c472caed22f1c011ba7bcaf9
Helga100 <ovargn25 @gmail.com>
2023-03-07 22:43:00 +0200

    git init

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Student pupil = new Student("Masha", 10, 10.5);
+        System.out.println(pupil.age);
+        System.out.println(pupil.grade);
+        System.out.println(pupil.name);
+
+    }
+}
